547840dd1223efea9205f89f93604cf900838b9a
inofinitivesr <inofinitivesr12.com>
2025-07-16 17:52:39 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,11 +94,11 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href=""
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <ImBlog style={{ marginBottom: "2px" }} />
               </Nav.Link>
             </Nav.Item>
 
