3a732462a85ad5f6d08499de3b49ff348e72e81f
inofinitivesr <inofinitivesr12.com>
2025-07-28 20:26:47 +0530

    project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -65,14 +65,14 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
+                    <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              title="AI For Social Good"
+              description="Using 'Natural Language Processing' for the detection of suicide-related posts and users' suicide ideation in cyberspace, thus helping in suicide prevention."
               ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
             />
           </Col>
 
@@ -81,10 +81,9 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              description="Trained a CNN classifier using the 'FER-2013 dataset' with Keras and TensorFlow backend. The classifier successfully predicted various types of human emotions, achieving a highest accuracy of 60.1%. Then used OpenCV to detect the face in an image and passed the face to the classifier to predict the person's emotion."
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
         </Row>
